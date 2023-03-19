f9c46988968f3240e9f7290a7d1f9da62df2b87a
Яна Бубнова <bujhvh@mail.ru>
2023-03-19 20:57:50 +0300

    main functions

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,13 +1,26 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+from collections import defaultdict
 
-
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+from datetime import datetime as dt
+from pep_parse.settings import BASE_DIR
 
 
 class PepParsePipeline:
+    def open_spider(self, spider):
+        self.statuses = defaultdict(int)
+
     def process_item(self, item, spider):
+        self.statuses[item["status"]] += 1
         return item
+
+    def close_spider(self, spider):
+        print('BASE_DIR', BASE_DIR)
+        with open(
+            f'{BASE_DIR}/results/status_summary_{dt.now().strftime("%Y-%m-%d_%H-%M-%S")}.csv', mode='w', encoding='utf-8'
+        ) as f:
+            f.write("Статус,Количество\n")
+            for key, value in self.statuses.items():
+                f.write(f"{key},{value}\n")
+            f.write(f"Всего,{sum(self.statuses.values())}\n")
+
+
+# status_summary_2029-01-31_23-55-00.csv
--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,10 +1,25 @@
 import scrapy
 
+from pep_parse.items import PepParseItem
+
 
 class PepSpider(scrapy.Spider):
-    name = 'pep'
-    allowed_domains = ['peps.python.org']
-    start_urls = ['http://peps.python.org/']
+    name = "pep"
+    allowed_domains = ["peps.python.org"]
+    start_urls = ["https://peps.python.org/"]
 
     def parse(self, response):
-        pass
+        for link in response.css(
+            "section#numerical-index tbody tr td a::attr(href)"
+        ).getall():
+            yield response.follow(link, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        number, name = response.css("h1.page-title::text").get().split(" – ")
+        status = response.css('dt:contains("Status") + dd abbr::text').get()
+        data = {
+            "number": number.split(" ")[1],
+            "name": name,
+            "status": status,
+        }
+        yield PepParseItem(data)
